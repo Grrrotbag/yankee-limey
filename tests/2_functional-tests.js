1432f120183c138d80542efae432c0807b17752e
Gerard Keane <gerard.keane@dfkeane.com>
2021-03-21 23:51:45 +0000

    comments for functional tests to be written

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,4 +7,11 @@ chai.use(chaiHttp);
 
 let translator = require("../components/translator.js");
 
-suite("Functional Tests", () => {});
+suite("Functional Tests", () => {
+  // Translation with text and locale fields: POST request to /api/translate
+  // Translation with text and invalid locale field: POST request to /api/translate
+  // Translation with missing text field: POST request to /api/translate
+  // Translation with missing locale field: POST request to /api/translate
+  // Translation with empty text: POST request to /api/translate
+  // Translation with text that needs no translation: POST request to /api/translate
+});
