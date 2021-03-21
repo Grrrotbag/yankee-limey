896b372b07ca9a1c52e771968e7c4dc2c9609c04
Gerard Keane <gerard.keane@dfkeane.com>
2021-03-21 18:06:17 +0000

    api returns json

--- routes/api.js
+++ routes/api.js
@@ -6,9 +6,7 @@ module.exports = function (app) {
   const translator = new Translator();
 
   app.route("/api/translate").post((req, res) => {
-    // console.log(req.body);
-    console.log(translator.translate(req.body));
-
-    return translator.translate(req.body);
+    let responseObj = translator.translate(req.body);
+    return res.json({ text: responseObj.text, translation: responseObj.translation });
   });
 };
